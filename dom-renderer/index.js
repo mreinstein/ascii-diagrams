b3b7a57bf84425fe57bb4adbe95460e59d0e3863
Mike Reinstein <mreinstein@sellpoints.com>
2020-01-14 15:52:29 -0800

    remove snabby rendering in favor of pure dom updates

--- dom-renderer/index.js
+++ dom-renderer/index.js
@@ -1,25 +1,38 @@
-import html from 'snabby'
-
-
 const _grid = {
 	columns: 100,
 	rows: 54,
 	data: [ ]
 }
 
+
+const changed = [ ]
+const cellEls = [ ]
+
 const chars = [ '▼', '▲', '◀', '▶', '┤', '├', '┴', '┬', ' ', '|', '-', ' ', '┘', '└', '┌', '┐', ' ', ' ', ' ', ' ' ]
 
 for (let i=0; i < _grid.columns * _grid.rows; i++) {
 	const val = chars[Math.floor(Math.random() * chars.length)]
 	_grid.data.push(val)
+	changed.push(i)
 }
 
 
-let vNode = document.getElementById('grid')
-
-
 function render (grid) {
-	const result = [ ]
+	for (let i=0; i < changed.length; i++) {
+		const idx = changed[i]
+		//const col = idx % grid.columns
+		//const row = idx / grid.columns | 0
+		cellEls[idx].innerText = grid.data[idx]
+	}
+
+	changed.length = 0
+}
+
+
+function createGrid (grid) {
+
+	const gridEl = document.getElementById('grid')
+	let result = ''
 
 	const bgColor = 'white'
 	const fgColor = 'black'
@@ -27,13 +40,31 @@ function render (grid) {
 	for (let row = 0; row < grid.rows; row++) {
 		for (let col=0; col < grid.columns; col++) {
 			const cell = (row * grid.columns) + col
-			result.push(html`<div style="color: ${fgColor}; background-color: ${bgColor}; width: 9px;">${grid.data[cell]}</div>`)
+			const nextEl = document.createElement('div')
+			nextEl.style.color = fgColor
+			nextEl.style.backgroundColor = bgColor
+			nextEl.style.width = '9px'
+			nextEl.innerText = grid.data[cell]
+			gridEl.appendChild(nextEl)
+			cellEls.push(nextEl)
 		}
-		result.push(html`<br>`)
+		gridEl.appendChild(document.createElement('br'))
+		//result += `<br>`
 	}
 
-	vNode = html.update(vNode, html`<div id="grid" class="unicodetiles">${result}</div>`)
+	//gridEl.innerHTML = result
 }
 
 
-render(_grid)
+/*
+setInterval(function () {
+	for (let i=0; i < 100; i++) {
+		const idx = Math.floor(Math.random() * _grid.columns * _grid.rows)
+		_grid.data[idx] = chars[Math.floor(Math.random() * chars.length)]
+		changed.push(idx)
+	}
+	render(_grid)
+}, 100)
+*/
+
+createGrid(_grid)
