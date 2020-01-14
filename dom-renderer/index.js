d3de94f03ef59683f59177525f8d083d50720a9c
Mike Reinstein <mreinstein@sellpoints.com>
2020-01-14 14:47:03 -0800

    initial work on dom renderer

--- dom-renderer/index.js
+++ dom-renderer/index.js
@@ -1,7 +1,35 @@
-
-// TODO: pull in snabby to make this easier
-
-function render () {
-
+const _grid = {
+	columns: 160,
+	rows: 54,
+	data: [ ]
 }
 
+const chars = [ '▼', '▲', '◀', '▶', '┤', '├', '┴', '┬', ' ', '|', '-', ' ', '┘', '└', '┌', '┐', ' ', ' ', ' ', ' ' ]
+
+for (let i=0; i < _grid.columns * _grid.rows; i++) {
+	const val = chars[Math.floor(Math.random() * chars.length)]
+	_grid.data.push(val)
+}
+
+
+// TODO: investigate snabby for rendering
+function createGrid (grid) {
+	let result = ''
+
+	const bgColor = 'white'
+	const fgColor = 'black'
+
+	for (let row = 0; row < grid.rows; row++) {
+		for (let col=0; col < grid.columns; col++) {
+			const cell = (row * grid.columns) + col
+			result += `<div style="color: ${fgColor}; background-color: ${bgColor}; width: 9px;">${grid.data[cell]}</div>`
+		}
+		result += '<br>'
+	}
+
+	const gridEl = document.getElementById('grid')
+	gridEl.innerHTML = result
+}
+
+
+createGrid(_grid)
