72ed00c11058f9731ee97cb0d9c256e2a5a1096c
Mike Reinstein <mreinstein@sellpoints.com>
2020-03-21 20:31:55 -0700

    minify the source

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
-import commonjs from 'rollup-plugin-commonjs'
-import replace  from 'rollup-plugin-replace'
-import resolve  from 'rollup-plugin-node-resolve'
+import commonjs   from 'rollup-plugin-commonjs'
+import replace    from 'rollup-plugin-replace'
+import resolve    from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 
 
 const config = [
@@ -16,6 +17,13 @@ const config = [
       commonjs(),
       replace({
         'process.env.NODE_ENV': `'production'`
+      }),
+      terser({
+        sourcemap: true,
+        numWorkers: 1,
+        output: {
+          ecma: 6
+        }
       })
     ]
   }
